8dc49ca31cc46698c593bbcd5e86d44d6d11ef21
unknown <ak833961@gmail.com>
2024-01-29 12:45:08 +0500

    :files

---
+++ src/content_option.js
@@ -0,0 +1,151 @@
+const logotext = "Aman Khan";
+const meta = {
+  title: "Aman Khan",
+  description:
+    "I’m Aman khan data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Aman Khan",
+  animated: {
+    first: "Coding is my Passion",
+    second: "Programming is thinking not typing",
+    third: "its not a bug its an undocumented feature",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: "https://pics.craiyon.com/2023-11-21/TaH1PVumRqaO9_PVcxhU3A.webp",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "React JS",
+    value: 90,
+  },
+  {
+    name: "Tailwind CSS",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Bootstrap",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+  {
+    name: "HTML / CSS",
+    value: 100,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRTxrEtWromdYwl85UC4J-AGBrkm3MY6D79w&usqp=CAU",
+    description:
+      "Tesla car Website",
+    link: "https://www.tesla.com/",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmg0z3LdlVtFd_LVMEU5Xw0hLSr2jA26DsLw&usqp=CAU",
+    description:
+      "NEOREACH website",
+    link: "https://neoreach.com/",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg13rTaHtZ0YIqD_Z8tihI8gArdrmgUOTTeA&usqp=CAU",
+    description:
+      "Techno website",
+    link: "https://techno.dreamitsolution.net/",
+  },
+  {
+    img: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs3/323129369/original/1dd8945467c54148b95cbcae9b6e3457f554ba0e/deliver-ekommart-shopify-theme-full-installation-and-customization.png",
+    description:
+      "ELLA GYM website",
+    link: "https://github.com/Amankhan315/Ellagym",
+  },
+  {
+    img: "https://halothemes.net/cdn/shop/products/ella-home-14-pc.jpg?v=1674746787",
+    description:
+      "ELLA WHEEL website",
+    link: "https://new-ella-demo.myshopify.com/?fts=0",
+  },
+  
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "ak833961@gmail.com",
+  YOUR_FONE: "03316905669",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
